03e6446fb3decbe9c6555981672b0a2b9bcc7ea3
Caio Cordeiro <ccmr@cesar.school>
2018-09-13 16:54:46 -0300

    TA FUNCIONANDO

--- CaioCordeiro.c
+++ CaioCordeiro.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
-typedef struct Noo{
+#include <stdlib.h>
+struct No{
     int info;
-    No *esquerda;
-    No *direita;
-    No *pai;
-}No;
-No *criararvore(int info,No* esq,No* dir){
-    No *raiz;
-    raiz = malloc(sizeof(No));
+    struct No *esquerda;
+    struct No *direita;
+    struct No *pai;
+
+};
+struct No *criararvore(int info){
+    struct No *raiz;
+    raiz = (struct No *) malloc(sizeof(struct No));
     raiz->info = info;
-    raiz->esquerda = esq;
-    raiz->direita = dir;
-    esq->pai=raiz;
-    dir->pai=raiz;
+    raiz->esquerda = NULL;
+    raiz->direita = NULL;
     return raiz; 
 }
-No *inserir(int info,No *arvore){
-    if(info<arvore->info){
+struct No *inserir(int info,struct No *arvore){
+    
+    if(arvore==NULL){
+        arvore=criararvore(info);
+    }
+    else if(info<arvore->info){
         arvore->esquerda = inserir(info,arvore->esquerda);
         arvore->esquerda->pai = arvore;
     }
@@ -26,25 +30,60 @@ No *inserir(int info,No *arvore){
     }
     return arvore;
 }
-int preordem(No *x){
-    if(x->info!= NULL){
-        printf("%d",x->info);
+int preordem(struct No *x){
+    if(x!= NULL){
+        printf(" %d",x->info);
         preordem(x->esquerda);
         preordem(x->direita);
     }
     return 0;
 }
-int inordem(No *x){
-    if(x->info !=NULL){
+int inordem(struct No *x){
+    if(x!=NULL){
         inordem(x->esquerda);
-        printf("%d",x->info);
+        printf(" %d",x->info);
         inordem(x->direita);
     }
 }
-int posordem(No *x){
-    if(x->info!=NULL){
+int posordem(struct No *x){
+    if(x!=NULL){
         posordem(x->esquerda);
         posordem(x->direita);
-        printf("%d",x->info);
+        printf(" %d",x->info);
     }
+}
+int main(){
+ int c;
+ int i = 0;
+ int j = 0;
+ int n;
+ int values;
+ struct No *arv=NULL;
+
+
+    scanf("%d",&c);
+    while(i<c){
+        printf("Case %d:",(i+1));
+        printf("\n");
+        scanf("%d",&n);
+        while(j<n){
+            scanf("%d",&values);
+            arv = inserir(values,arv);
+            j=j+1;
+        }
+    printf("Pre.:");
+    preordem(arv);
+    printf("\n");
+    printf("In..:");
+    inordem(arv);
+    printf("\n");
+    printf("Post:");
+    posordem(arv);
+    printf("\n");
+    printf("\n");
+    arv = NULL;
+    j = 0;
+    i=i+1;
+    }
+    return 0;
 }
